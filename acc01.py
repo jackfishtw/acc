07edc17f917bc1c006913f04cdb3e419f912d729
Jack Fish <jackfishtw@gmail.com>
2018-03-26 16:58:49 +0800

    測試for loop 可以用， 把弄成註解的while loop刪掉

--- acc01.py
+++ acc01.py
@@ -27,24 +27,6 @@ def txt2xls(filename,xlsname):  #txt轉excel的函式，filename 是要被轉的
     sheet.write(0,7,'摘要')
     sheet.write(0,8,'股票代號')
     sheet.write(0,9,'摘要明細')
-    '''
-    while True:  #開一個無限迴圈
-        line = f.readline() #讀文字檔的一行
-        if not line:  #如果讀不到一行，就結束迴圈
-            break        
-        if line[0:2] == '20':
-            sheet.write(x,0,line[0:8].strip())      #交易日
-            sheet.write(x,1,line[8:12].strip())     #序號
-            sheet.write(x,2,line[12:16].strip())    #時間
-            sheet.write(x,3,line[16:23].strip())    #票號
-            sheet.write(x,4,line[23:39].strip())    #支出金額
-            sheet.write(x,5,line[39:55].strip())    #收入金額
-            sheet.write(x,6,line[55:71].strip())    #餘額
-            sheet.write(x,7,line[71:79].strip())    #摘要
-            sheet.write(x,8,line[79:85].strip())    #股票代號
-            sheet.write(x,9,line[85:].strip())      #摘要明細
-            x += 1 #excel 要寫的行號加1
-            '''
     for line in f:
         if line[0:2] == '20':
             sheet.write(x,0,line[0:8].strip())      #交易日
